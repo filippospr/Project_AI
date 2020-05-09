1bc363fb9e7c22a6335a04e09812c3378afc9d05
nickp3065 <47462183+nickp3065@users.noreply.github.com>
2020-05-09 22:48:50 +0300

    fix 2 mistakes and put more comments

--- UCS.java
+++ UCS.java
@@ -3,37 +3,44 @@ import java.util.ArrayList;
 import java.lang.Math;
 
 public class UCS {
-	ArrayList<State> frontier = new ArrayList<State>(); 
+	ArrayList<State> frontier = new ArrayList<State>();
 
 	public void addToFrontier(State state){
 		int size = frontier.size();
-		if (size == 0)
+		//for empty frontier
+		if (size == 0){
 			frontier.add(state);
+			return;
+		}
 
 		for (int i=0; i<size; i++) {
 			if (state.cost < frontier.get(i).cost) {
-				frontier.add(i, state);
+				frontier.add(i, state);//found pos of state
 				break;
 			}
 		}
 	}
+
 	/*Removes the first element of the frontier since it is sorted*/
 	public State removeFromFrontier(){
 		if (frontier.size() == 0)
-			return null;
+			return null;//frontier empty return null
 		return frontier.remove(0);
 	}
 
 	public void createChildren(State state){
 		ArrayList<State> children = new ArrayList<State>();
 		int pos = state.state.indexOf('-');
+
 		for (int i=0; i<state.state.length(); i++) {
 			char[] child = state.state.toCharArray();
 			if (i != pos){
+				//Swap '-' with another character
 				char temp = child[i];
 				child[i] = child[pos];
 				child[pos] = temp;
-				int cost = Math.abs(pos-i);
+				//calculate cost
+				int cost = state.cost + Math.abs(pos-i);
 				children.add(new State(new String(child),cost));
 			}
 		}
@@ -41,9 +48,9 @@ public class UCS {
 	}
 
 	public static void main(String[] args) {
-		/* Get Starting state */
+		/* Get Starting state
 		Scanner scanner = new Scanner(System.in);
-		/*System.out.println("Give me a starting state: ");
+		System.out.println("Give me a starting state: ");
 		String state = scanner.nextLine();*/
 
 		UCS mak = new UCS();
