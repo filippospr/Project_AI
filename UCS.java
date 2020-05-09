d2b73a583d60856324ae450ad43d03908996e77e
nickp3065 <47462183+nickp3065@users.noreply.github.com>
2020-05-09 18:29:40 +0300

    First commit

---
+++ UCS.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class UCS {
+	ArrayList<State> frontier = new ArrayList<State>(); 
+
+	public void addToFrontier(State state){
+		int size = frontier.size();
+		if (size == 0)
+			frontier.add(state);
+
+		for (int i=0; i<size; i++) {
+			if (state.cost < frontier.get(i).cost) {
+				frontier.add(i, state);
+				break;
+			}
+		}
+	}
+	/*Removes the first element of the frontier since it is sorted*/
+	public State removeFromFrontier(){
+		if (frontier.size() == 0)
+			return null;
+		return frontier.remove(0);
+	}
+
+	public void createChildren(State state){
+		ArrayList<State> children = new ArrayList<State>();
+		int pos = state.state.indexOf('-');
+		for (int i=0; i<state.state.length(); i++) {
+			char[] child = state.state.toCharArray();
+			if (i != pos){
+				char temp = child[i];
+				child[i] = child[pos];
+				child[pos] = temp;
+				int cost = Math.abs(pos-i);
+				children.add(new State(new String(child),cost));
+			}
+		}
+		System.out.println(children);
+	}
+
+	public static void main(String[] args) {
+		/* Get Starting state */
+		Scanner scanner = new Scanner(System.in);
+		/*System.out.println("Give me a starting state: ");
+		String state = scanner.nextLine();*/
+
+		UCS mak = new UCS();
+		State testState = new State("MMAMMA-AM", 10);
+		mak.addToFrontier(testState);
+		mak.createChildren(testState);
+		System.out.println();
+	}
+
+	static class State{
+		String state;
+		int cost;
+
+		State(String state, int cost){
+			this.state = state;
+			this.cost = cost;
+		}
+
+		public String toString(){
+			return state+':'+cost;
+		}
+	}
+}
