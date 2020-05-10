e34568ce1cdf82573541529f18f708586993daf1
nickp3065 <47462183+nickp3065@users.noreply.github.com>
2020-05-10 19:42:26 +0300

    Update UCS.py

--- UCS.py
+++ UCS.py
@@ -1,16 +1,23 @@
 import random, sys, os
 
 def shuffle(n):
-	n = int(sys.argv[1])
 	l = ["M" for i in range(n)] + ["A" for i in range(n)] + ["-"]
 	random.shuffle(l)
 	return "".join(l)
 
-if __name__ == '__main__':
-	s = shuffle(sys.argv[1])
+def main():
+	if len(sys.argv) != 2:
+		print('Give an number n as an argument')
+		return
+	else:
+		n = int(sys.argv[1])
+	s = shuffle(n)
 	print(s)
 	with open("string.txt", "w") as f:
 		f.write(s)
 	os.system("javac UCS.java")
 	os.system("java UCS < string.txt")
 	os.remove("string.txt")
+
+if __name__ == '__main__':
+	main()
