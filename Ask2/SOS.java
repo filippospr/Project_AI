3c7fa7e26529b741ceeac4b4ba97897eefc14902
nickp3065 <47462183+nickp3065@users.noreply.github.com>
2020-05-21 18:50:42 +0300

    Update SOS.java

--- Ask2/SOS.java
+++ Ask2/SOS.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.lang.Math;
+import java.util.Scanner;
 
 public class SOS {
 
@@ -52,19 +53,62 @@ public class SOS {
 
 
 	public static void main(String[] args) {
+		System.out.println("Building Game Tree...");
 		char[][] startingState = new char[][]{{' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '}};
 		State gameState = new State(startingState, true, new int[]{1,2}, 'O');
-		gameState.printState();
 		createGameTree(gameState);
 		Scanner input = new Scanner(System.in);
+		gameState.printState();
+		
+		if (args.length == 2){
+			if (args[1].equals("-t")){
+				return;
+			}
+			for(int i=0; i<Integer.parseInt(args[1]);i++){
+				test(gameState);
+			}
+			return;
+		}
 
 		while (!gameState.isFinal){
-			System.out.println("Plz enter S or O and its coordinates (ex. O 1 2)");
-			char c = input.nextChar();
-			int[] pos = new int[]{input.nextInt(), input.nextInt()};//TODO: check if invalid not S/O or bad coordinates
+			System.out.println("AI's turn");
+			int maxValue = gameState.children.get(0).value;
+			State maxChild = gameState.children.get(0);
 			
+			//find the child with max value
+			System.out.println(gameState.isAI);
+			for (State child: gameState.children){
+				System.out.println(child.isAI);
+				if (child.value > maxValue){
+					maxValue = child.value;
+					maxChild = child;
+					break;
+				}
+			}
+			gameState = maxChild;
+			gameState.printState();
+			if (gameState.isFinal)
+				break;
 
+			System.out.println("Plz enter S or O and its coordinates (ex. O 1 2)");
+			char c = input.next().charAt(0);
+			int[] pos = new int[]{input.nextInt(), input.nextInt()};//TODO: check if invalid not S/O or bad coordinates
+			for (State child: gameState.children){
+				if (child.grid[pos[0]][pos[1]] == c){
+					gameState = child;
+					break;
+				}
+			}
+			gameState.printState();
 		}
+
+		//game over
+		if (gameState.value == 1)
+			System.out.println("AI Wins!");
+		else if (gameState.value == -1)
+			 System.out.println("Player wins!!");
+		else
+			System.out.println("It's a draw");
 
 		//System.out.println(root.value);
 		//System.out.printf("Nodes=%d\nAI_Wins=%d\n",nodes,aiWins);
@@ -74,6 +118,38 @@ public class SOS {
 			System.out.println("--------------------------------------------");
 			child.printState();
 		}*/
+	}
+
+	public static void test(State gameState){
+		while (!gameState.isFinal){
+			int maxValue = gameState.children.get(0).value;
+			State maxChild = gameState.children.get(0);
+			
+			//find the child with max value
+			for (State child: gameState.children){
+				if (child.value > maxValue){
+					maxValue = child.value;
+					maxChild = child;
+					break;
+				}
+			}
+			gameState = maxChild;
+			if (gameState.isFinal)
+				break;
+			maxValue = gameState.children.get(0).value;
+			maxChild = gameState.children.get(0);
+			
+			//find the child with max value
+			for (State child: gameState.children){
+				if (child.value < maxValue){
+					maxValue = child.value;
+					maxChild = child;
+					break;
+				}
+			}
+			gameState = maxChild;
+		}
+		System.out.println(gameState.value);
 	}
 
 	static class State {
@@ -119,8 +195,8 @@ public class SOS {
 								if (grid[x][y] == 'S') {
 									isFinal = true;
 									//1 AI wins -1 player wins
-									if (isAI) value = 1;
-									else value = -1;
+									if (isAI) value = -1;
+									else value = 1;
 									
 									return;
 								}
